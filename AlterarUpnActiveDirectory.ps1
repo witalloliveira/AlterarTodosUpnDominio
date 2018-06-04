dcdb05ec7ac5558e1bdbec841ad8dd45c4c25997
Witallo Oliveira <witallo@outlook.com>
2018-06-04 16:49:38 -0300

    UPN

---
+++ AlterarUpnActiveDirectory.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module ActiveDirectory
+
+$oldSuffix = "diegomatos.local"
+$newSuffix = "labcloud2go.com.br"
+
+$OU = "OU=Temp,DC=diegomatos,DC=local"
+$server = "DCM1"
+
+Get-ADUser -SearchBase $OU -filter * | ForEach-Object {
+$newUPN = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
+$_ | Set-ADUser -server $server -UserPrincipalName $newUPN
+}
